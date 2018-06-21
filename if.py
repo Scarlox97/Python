cd07e75b54a6322ede42cd1c1e78bcb3e41f0678
Scarlox97 <40360600+Scarlox97@users.noreply.github.com>
2018-06-21 20:38:23 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,36 @@
+import  commands,time
+
+option='''
+Press 1 to check Internet cable on your machine :
+Press 2 to check internet access 
+Press 3 to check for google acces
+'''
+print option
+
+choice=raw_input()
+
+if   choice  ==  '1'    :
+
+    print  "plz wait internet cable is being checked "
+    time.sleep(3)
+    cable_check=commands.getoutput('sudo mii-tool  eno1')
+    if  'link ok' in  cable_check:
+        print  "Lan cable is Connected"
+
+    else :
+        print "Cable is'nt Connected"
+
+
+
+
+elif    choice  ==  '2' :
+
+    print  "Internet connectivity is checking in a while"
+
+elif  choice  ==  '3'  :
+
+    print  "loading google web page "
+
+else :
+    print  "wrong option"
+    
